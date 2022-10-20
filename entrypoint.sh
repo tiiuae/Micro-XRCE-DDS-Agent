b864988e2768098fd8f2f3c104cd7df5ed0ed5db
Jari Nippula <jarix@ssrc.tii.ae>
2022-10-20 10:20:13 +0300

    entrypoint to unset FASTRTPS_DEFAULT_PROFILES_FILE

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+unset FASTRTPS_DEFAULT_PROFILES_FILE
+
 ./parse_agent_refs.py
 
-MicroXRCEAgent udp4 --port 2020 --send_port 2019 --refs /agent.refs
+MicroXRCEAgent udp4 --port 2020 --send_port 2019 --refs /enclave/agent.refs
