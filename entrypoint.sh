67b9a44d6fc561ee0be6366240042a1a0a73e08b
Timo Sairiala <timo.sairiala@unikie.com>
2022-11-04 16:50:31 +0200

    Add SIGTERM handler for docker initial process

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,24 @@
 #!/bin/bash -e
 
 source /opt/ros/galactic/setup.bash
+_term() {
+        # FILL UP PROCESS SEARCH PATTERN HERE TO FIND PROPER PROCESS FOR SIGINT:
+        pattern="MicroXRCEAgent"
+
+        pid_value="$(ps -ax | grep $pattern | grep -v grep | awk '{ print $1 }')"
+        if [ "$pid_value" != "" ]; then
+                pid=$pid_value
+                echo "Send SIGINT to pid $pid"
+        else
+                pid=1
+                echo "Pattern not found, send SIGINT to pid $pid"
+        fi
+        kill -s SIGINT $pid
+}
+trap _term SIGTERM
 
 MicroXRCEAgent udp4 --port 2020 --send_port 2019 --refs /agent.refs
+
+child=$!
+echo "Waiting for pid $child"
+wait $child
